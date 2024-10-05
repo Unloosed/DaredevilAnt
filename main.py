fefb31e2c6266fa2c6362a502d1e854c41955cb8
Michael Evans <77376183+Unloosed@users.noreply.github.com>
2024-10-05 15:35:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 import pygame
 from menu import main_menu
+from music import play_main_theme
 
 # Initialize Pygame
 pygame.init()
 
 if __name__ == "__main__":
+    play_main_theme()
     main_menu()
